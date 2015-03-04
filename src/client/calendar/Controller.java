0575bddd92716c0d94156425c9fd06d7022cf0c6
Jonas Løchsen <jonas.lochsen@gmail.com>
2015-03-04 13:50:28 +0100

    button names

--- src/client/calendar/Controller.java
+++ src/client/calendar/Controller.java
@@ -2,8 +2,10 @@ package client.calendar;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -12,8 +14,14 @@ import javafx.stage.Stage;
 public class Controller {
 
 
-    @FXML
-    private ComboBox chooseCalendar;
+    @FXML private ComboBox chooseCalendar;
+    @FXML private Text name;
+    @FXML private Button logOff;
+    @FXML private Button userSettings;
+    @FXML private Button yourCalendar;
+    @FXML private Button today;
+
+
 
     protected static Stage primaryStage;
 
@@ -30,5 +38,13 @@ public class Controller {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void logOff(ActionEvent event) {
+        //client.login.Main.show(Main.stage);
+    }
+
+    public void showUserSettings(ActionEvent event) {
+
     }
 }
