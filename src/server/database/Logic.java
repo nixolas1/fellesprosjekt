654118d12549906dbb2c8c9387632e80555d30b4
Nicolas A. Tonne <nixatto@gmail.com>
2015-03-05 11:26:32 +0100

    shit

--- src/server/database/Logic.java
+++ src/server/database/Logic.java
@@ -54,11 +54,12 @@ public class Logic {
     }
 
     public static ArrayList<List<String>> getAllRows(String table) {
-        return getAllRowsWhere(table, "");
+        return getAllRowsWhere(table, null);
     }
 
     public static ArrayList<List<String>> getAllRowsWhere(String table, String where){
-        String getRow = "SELECT * FROM " + table + where + ";";
+        String whereText = where != null ? " WHERE "+where : "";
+        String getRow = "SELECT * FROM " + table + whereText + ";";
         String[] row = new String[getNumberOfColumns(table)];
         Statement stmt = null;
         ResultSet result = null;
