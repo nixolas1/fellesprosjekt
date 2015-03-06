fa87f67d1a1cba026b2d6abb25eea9a14fc9add8
Jonas Løchsen <jonas.lochsen@gmail.com>
2015-03-06 14:03:55 +0100

    Main GUI grid change and text constraint

--- src/client/calendar/Controller.java
+++ src/client/calendar/Controller.java
@@ -8,6 +8,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+
+
 /**
  * Created by jonaslochsen on 02.03.15.
  */
@@ -20,8 +23,18 @@ public class Controller {
     @FXML private Button userSettings;
     @FXML private Button yourCalendar;
     @FXML private Button today;
+    @FXML private Text weekNum;
+    @FXML private Text monDate;
+    @FXML private Text tueDate;
+    @FXML private Text wedDate;
+    @FXML private Text thuDate;
+    @FXML private Text friDate;
+    @FXML private Text satDate;
+    @FXML private Text sunDate;
 
 
+
+    private LocalDate displayDate = LocalDate.now();
 
     protected static Stage primaryStage;
 
@@ -30,6 +43,12 @@ public class Controller {
         chooseCalendar.setItems(FXCollections.observableArrayList("Gunnar Greve"));
 
     }
+
+    public void updateDate() {
+
+    }
+
+
 
     public void onBtnShowNewAppointment(ActionEvent event) {
         client.newAppointment.Main newApp = new client.newAppointment.Main();
