6cbcff5b8498a83d63678749103a6e5879291939
apefisk <mn.94@hotmail.com>
2015-03-08 18:44:52 +0100

    new, but not functional version

--- src/client/newGroupCalendar/Main.java
+++ src/client/newGroupCalendar/Main.java
@@ -1,9 +1,12 @@
 package client.newGroupCalendar;
 
+import calendar.UserModel;
+import client.newAppointment.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -11,10 +14,14 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private Stage myParent;
+    private Stage newGroupStage;
+    private UserModel loggedUser;
+
     @Override
     public void start(Stage primaryStage) {
         try {
-            GridPane root = (GridPane) FXMLLoader.load(Main.class.getResource("gui.fxml"));
+            GridPane root = (GridPane) FXMLLoader.load(Main.class.getResource("groupGui.fxml"));
             Scene scene = new Scene(root,500,600);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -28,4 +35,22 @@ public class Main extends Application {
         launch(args);
     }
 
+    public void showNewGroup(Stage parentStage, UserModel loggedUser) {
+        this.myParent = parentStage;
+        this.loggedUser = loggedUser;
+
+        try {
+            newGroupStage = new Stage();
+            GridPane pane = (GridPane) FXMLLoader.load(Controller.class.getResource("groupGui.fxml"));
+            Scene scene = new Scene(pane);
+            newGroupStage.setScene(scene);
+            newGroupStage.setTitle("Ny gruppe");
+            newGroupStage.initOwner(this.myParent);
+            newGroupStage.initModality(Modality.WINDOW_MODAL);
+            newGroupStage.show();
+        } catch (Exception ex) {
+            System.out.println("Exception found in newGroup" + ex);
+            ex.printStackTrace();
+        }
+    }
 }
