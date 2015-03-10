e4e2d277e3204425e004b47707743f316b9c5e82
Nicolas A. Tonne <nixatto@gmail.com>
2015-03-10 09:33:17 +0100

    Model fixes

--- src/calendar/Notification.java
+++ src/calendar/Notification.java
@@ -7,19 +7,28 @@ import java.util.Date;
  * Created by nixo on 3/4/15.
  */
 public class Notification implements Serializable {
-    Appointment app;
-    Calendar cal;
-    UserModel user;
-    String text;
-    boolean seen;
-    Date sent;
+    public Appointment app;
+    public Calendar cal;
+    public UserModel user;
+    public String text;
+    public boolean seen;
+    public Date sent;
+    public Date dateSeen;
 
-    public Notification(Appointment app, Calendar cal, UserModel user, String text, boolean seen, Date sent){
+    public Notification(String text, UserModel user, boolean seen){
+        this.text=text;
+        this.seen=seen;
+        this.user=user;
+    }
+
+    public Notification(Appointment app, Calendar cal, UserModel user, String text, boolean seen, Date sent, Date dateSeen){
         this.app=app;
         this.cal=cal;
         this.user=user;
         this.text=text;
         this.seen=seen;
         this.sent=sent;
+        this.dateSeen=dateSeen;
     }
+
 }
